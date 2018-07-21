4c5d2bb6946cc40da57a2c3cf58e67097dd13de9
James Stephens <stephensja7@gmail.com>
2018-07-21 13:35:06 -0400

    added solution

---
+++ 2. Bootcamp Prep/01-tidy-code/01-style-me-pretty.js
@@ -0,0 +1,22 @@
+function styleMePretty() {
+  let truth = '';
+
+  function innerBeauty(count) {
+    let result = '';
+
+    while (count > 0) {
+      count--;
+      if (count===2) {
+        result += 'Unformatted code';
+      }
+    }
+
+    result += ' is difficult to parse';
+
+    return result;
+  }
+
+  truth += `${innerBeauty(5)} for humans if not for machines`;
+
+  return truth;
+}
