1c6348f81f559f59d53cadbfd77d52f479744ebd
James Stephens <stephensja7@gmail.com>
2018-07-21 13:39:52 -0400

    added solution

---
+++ 2. Bootcamp Prep/01-tidy-code/02-just-in-camel-case.js
@@ -0,0 +1,17 @@
+function justInCamelCase(underName) {
+  let camelCaseOutput = '';
+  let foundUnder = false;
+
+  for (let i = 0; i < underName.length; i++) {
+    if (underName[i] === '_') {
+      foundUnder = true;
+    } else if (foundUnder) {
+        camelCaseOutput += underName[i].toUpperCase();
+        foundUnder = false;
+    } else {
+      camelCaseOutput += underName[i];
+    }
+  }
+
+  return camelCaseOutput;
+}
