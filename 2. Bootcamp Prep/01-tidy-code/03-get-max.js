f0b68ecb7a31271ff99ec7dcb23cae791fcf3a39
James Stephens <stephensja7@gmail.com>
2018-07-21 13:43:28 -0400

    added solution

---
+++ 2. Bootcamp Prep/01-tidy-code/03-get-max.js
@@ -0,0 +1,12 @@
+function getMax(num1, num2, num3) {
+  if (num1 > num2) {
+    if (num1 > num3) {
+      return num1;
+    }
+    return num3;
+  } else if (num2 > num3) {
+    return num2;
+  } else {
+    return num3;
+  }
+}
