f6173c46921039eb2b055076ad89a33d1d4b32b6
James Stephens <stephensja7@gmail.com>
2018-07-21 11:36:13 -0400

    added solution

---
+++ 1. Pre-Work/04-control-flow/01-founder-finder.js
@@ -0,0 +1,10 @@
+let name = 'David';
+// let name = 'Nimit';
+// let name = 'Someone else';
+
+let found;
+
+if (name === 'David' || name === 'Nimit') found = true;
+else found = false;
+
+console.log(found);
