686aeff94596adb4ab65e7ba2046370404e34a64
James Stephens <stephensja7@gmail.com>
2018-07-22 17:19:02 -0400

    updated solution

--- 2. Bootcamp Prep/02-loops/02-crazy-caps.js
+++ 2. Bootcamp Prep/02-loops/02-crazy-caps.js
@@ -1,12 +1,5 @@
 const crazyCaps = (string) => {
-  let crazyString = '';
-
-  for (let i = 0; i < string.length; i++) {
-    if (i % 2 === 0) crazyString += string[i].toLowerCase();
-    if (i % 2 === 1) crazyString += string[i].toUpperCase();
-  }
-
-  return crazyString;
+  return string.split('').map((el, i) => (i % 2 === 1) ? el.toUpperCase() : el.toLowerCase()).join('');
 }
 
 console.log(crazyCaps('fullstack is amazing'));
