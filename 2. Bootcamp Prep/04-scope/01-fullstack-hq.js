a43b9e4fd829671eb99a58e84efee01e2aad7349
James Stephens <stephensja7@gmail.com>
2018-07-22 21:34:31 -0400

    added solution

---
+++ 2. Bootcamp Prep/04-scope/01-fullstack-hq.js
@@ -0,0 +1,20 @@
+let planet = 'Planet Earth';
+
+function fullstackHQ(){
+  return middleFunction('United States');
+
+  function middleFunction(country) {
+    return innerFunction('New York State');
+
+    function innerFunction(state) {
+      let city = 'New York City';
+      return innermostFunction();
+
+      function innermostFunction() {
+        return 'Fullstack HQ is at ' + planet + ', ' + country + ', ' + state + ', ' + city;
+      }
+    }
+  }
+}
+
+console.log(fullstackHQ());
