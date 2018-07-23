bfb60ec9547817a096a25d7dee19fc134185a59b
James Stephens <stephensja7@gmail.com>
2018-07-23 10:08:32 -0400

    added solution

---
+++ 2. Bootcamp Prep/05-arrays-i/02-my-includes.js
@@ -0,0 +1,4 @@
+const myIncludes = (arr, search) => (arr.indexOf(search) > -1) ? true : false;
+
+console.log(myIncludes([10, 20, 30], 20));
+// console.log(myIncludes(['apples', 'bananas', 'citrus'], 'pears'));
